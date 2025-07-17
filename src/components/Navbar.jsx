f7017c55ef19f2de3cb62fa9e5c8e3f729aadcea
purusottam32 <kingspuru@gmail.com>
2025-07-17 13:58:38 +0530

    FIX: UNiSALE logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate,NavLink } from 'react-router-dom';
+import Logo from '../assets/Logo.svg'; // Adjust the path as necessary
 
 
 function Navbar() {
@@ -19,15 +20,10 @@ function Navbar() {
    <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f1f4f1] px-10 py-3">
           <div className="flex items-center gap-8 container">
             <div className="flex items-center gap-4 text-[#131712]">
-              <div className="size-4">
-                <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M24 45.8096C19.6865 45.8096 15.4698 44.5305 11.8832 42.134C8.29667 39.7376 5.50128 36.3314 3.85056 32.3462C2.19985 28.361 1.76794 23.9758 2.60947 19.7452C3.451 15.5145 5.52816 11.6284 8.57829 8.5783C11.6284 5.52817 15.5145 3.45101 19.7452 2.60948C23.9758 1.76795 28.361 2.19986 32.3462 3.85057C36.3314 5.50129 39.7376 8.29668 42.134 11.8833C44.5305 15.4698 45.8096 19.6865 45.8096 24L24 24L24 45.8096Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </div>
-              <h2 className="text-[#131712] text-lg font-bold leading-tight tracking-[-0.015em]">Uni<span className='text-[#50d22c]'>Sell</span></h2>
+            <div className="mb-3">
+              <img src={Logo} width="120px" height="100px"alt="logo " />
+            </div>
+
             </div>
             <div className="flex items-center gap-9">
               <NavLink  className="text-[#131712] text-sm font-medium leading-normal" to={"/"}>Home</NavLink>
